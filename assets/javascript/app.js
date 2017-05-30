5882546689f77efd9738149ab554b4ae7a3159de
maria saavedra <mariasaavedra@marias-MacBook-Air.local>
2017-05-30 19:48:42 -0400

    for loop problem

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@
   var questionBank = [
     { indexOfQuestions: 0, question: "Who brought Jon Snow back to life?", answer: "Melisandre" },
     { indexOfQuestions: 1, question: "Who killed Joffrey?", answer: "Olenna Tyrell" },
-    { QobjectIndexer: 2, question: "Who is the real father of Cercei's kids?", answer: "Jaime Lannister" },
+    { indexOfQuestions: 2, question: "Who is the real father of Cercei's kids?", answer: "Jaime Lannister" },
     { indexOfQuestions: 3, question: "Who is Jon Snow's real mother?", answer: "Lyanna Stark" },
     { indexOfQuestions: 4, question: "Who 'drinks and knows things'?", answer: "Tyrionne Lannister" }
   ]
@@ -19,7 +19,6 @@ var wrongChoices = [
   ]
 
 
-
 //global variables 
 
 /* stores tally-counts into array */
@@ -28,42 +27,66 @@ var rightAnswers = 0;
 var wrongAnswers = 0;
 var unanswered = 0;
 
+//boolean 
+var hasChosen = false;
 
 var questionTime = 15;
+var answerTime = 7;
 var myInterval;
+
 var myCount;
 myCount = 0;
 
-
-
-function startGame () {
-    $(".btn").on("click", function () {
-
-        function timerForQuestion () {
-            $("#timeRemaining").html("<h2>Time Remaining: </h2>" + "" + myCount);
-
-            if(myCount > 16){
-                myInterval = setInterval(timerForAnswer, 1000);
-                myCount = 0;
-                timerForAnswer();
+    function timer (stopTime) {
+        //place the title and myCount to appear
+      $("#timeRemaining").html("<h2>Time Remaining: </h2>" + myCount);
+        
+        //insert function for picking and placing questions and choices
+        
+        if(myCount === stopTime) {
+            clearInterval(myInterval);
+            //show next question
+            //restart timer
+            var myTime;
+            if(stopTime === questionTime) {
+                myTime = answerTime;
+                
+                //to ensure to only click on one answer use the boolean "hasChosen = false;"
+                //if user has cliked the right answer, then show text that he is right and rightAnswers++;
+                //if user has cliked on the choices that are not the right answer, then show text that he is wrong and wrongAnswers++;
+                //else if he has not clicked on any choice at all tell he is did not answer and unanswered++;
+                //after that if statement change the boolean to "hasChosen === true"
             }else {
-                myCount++;
+                myTime = questionTime;
             }
+        myInterval = setInterval(timer, 1000, myTime);
+        myCount = 0;
+        }else {
+            myCount++;
         }
+        
+    };
 
-        function timerForAnswer () {
-            $("#timeRemaining").html("<h2>Time Remaining: </h2>" + "" + myCount);
+//callling the timer function to begin once the button is clicked
+$(".btn").on("click", function(){
+    myInterval = setInterval(timer, 1000, questionTime);
+    questionnaire(questionBank, wrongChoices);
+    
+});
 
-            if(myCount > 6) {
-                myInterval = setInterval(timerForQuestion, 1000);
-                myCount = 0;
-                timerForQuestion();
-            }else {
-                myCount++;
-            }
-        }
-
-
-    })
+function questionnaire (qAnswerArray, choicesArray) {
+    
+    for( var i = 0; i< 5; i++) {
+        
+        $("#question").append(qAnswerArray[i].question);//place question into the question area
+        choicesArray[i].push(qAnswerArray[i].answer); //include the answer choice inside of the wrong choices array 
+        $("#choices").append(choicesArray[i]);//place those choices (including the answer) here
+        break;
+    }
+    
+    
 };
-startGame();
+
+
+
+
